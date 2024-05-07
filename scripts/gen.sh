fa46c70ec00c39719f1c02851300e708131dcd2e
jeddygong <jed.gong.nine@gmail.com>
2024-05-07 11:28:31 +0800

    feat: 完善自动生成模版脚本

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -12,15 +12,9 @@ if [ "$#" -ne 1 ] || [[ $NAME =~ $re ]] || [ "$NAME" == "" ]; then
   exit 1
 fi
 
-DIRNAME="$FILE_PATH/src/$NAME"
 INPUT_NAME=$NAME
 
-# 1.判断组件是否存在
-if [ -d "$DIRNAME" ]; then
-  echo "$NAME 组件已经存在, 请更换后再试"
-  exit 1
-fi
-
+# 转成大写驼峰
 NORMALIZED_NAME=""
 for i in $(echo $NAME | sed 's/[_|-]\([a-z]\)/\ \1/;s/^\([a-z]\)/\ \1/'); do
   C=$(echo "${i:0:1}" | tr "[:lower:]" "[:upper:]")
@@ -28,42 +22,54 @@ for i in $(echo $NAME | sed 's/[_|-]\([a-z]\)/\ \1/;s/^\([a-z]\)/\ \1/'); do
 done
 NAME=$NORMALIZED_NAME
 
+DIRNAME="$FILE_PATH/src/$NAME"
+
+# 1.判断组件是否存在
+if [ -d "$DIRNAME" ]; then
+  echo "$INPUT_NAME 组件已经存在, 请更换后再试"
+  exit 1
+fi
+
 ############## 2.生成核心文件模版
 mkdir -p "$DIRNAME"
-mkdir -p "$DIRNAME/index.ts"
 mkdir -p "$DIRNAME/__tests__"
 
-cat > $DIRNAME/src/$INPUT_NAME.vue <<EOF
+cat > "${DIRNAME}/${NAME}.vue" <<EOF
 <script lang='ts' setup>
 // init here
 </script>
 <template>
-  ${INPUT_NAME}
+  <${NAME}>
     <slot></slot>
-  ${INPUT_NAME}
+  </${NAME}>
 </template>
 EOF
 
 cat > $DIRNAME/index.ts <<EOF
 export {
-  default as ${INPUT_NAME},
-} from './${INPUT_NAME}.vue'
+  default as ${NAME},
+} from './${NAME}.vue'
 EOF
 ############## 2.生成核心文件
 
 ##############  3.生成测试用例模版
-cat > $DIRNAME/__tests__/${INPUT_NAME}.vue <<EOF
+cat > $DIRNAME/__tests__/$NAME.vue <<EOF
+<script setup lang="ts">
+import { ref } from 'vue'
+import { $NAME } from '@/$NAME'
+
+</script>
 <template>
-  <${INPUT_NAME}>
+  <${NAME}>
     <div>组件运行</div>
-  </${INPUT_NAME}>
+  </${NAME}>
 </template>
 EOF
 
 cat > $DIRNAME/__tests__/index.test.ts <<EOF
 import { beforeEach, describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
-import $NAME from './${INPUT_NAME}.vue'
+import $NAME from './${NAME}.vue'
 import type { VueWrapper } from '@vue/test-utils'
 
 describe('$NAME Component', () => {
@@ -80,17 +86,20 @@ EOF
 ############## 3.生成测试用例
 
 ##############  4.生成文档模版
-mkdir -p "$DIRNAME/components/$INPUT_NAME"
 
-cat > $DOCS_PATH/components/$INPUT_NAME/index.md <<EOF
+cat > $DOCS_PATH/components/$INPUT_NAME.md <<EOF
 ---
-title: $INPUT_NAME
+title: $NAME
 ---
 
-# $INPUT_NAME
+# $NAME
 
 
 ## 基础用法
 EOF
 
 ##############  4.生成文档模版
+
+##############  5.在入口 index.ts 中追加导出
+echo "export * from './$NAME'" >> $FILE_PATH/src/index.ts
+##############  5.在入口 index.ts 中追加导出
