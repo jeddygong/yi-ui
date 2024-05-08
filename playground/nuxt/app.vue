46602633640b99ce8282e2db6940b6fa17198b16
jeddygong <jed.gong.nine@gmail.com>
2024-05-08 18:11:19 +0800

    test: 测试 nuxt3 运行组件

--- playground/nuxt/app.vue
+++ playground/nuxt/app.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <main>
+    <!-- compatibility -->
+    <Html lang="zh-TW">
+      <Head>
+        <Meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no"
+        />
+      </Head>
+    </Html>
+    <NuxtPage />
+  </main>
 </template>
+
+<style scoped>
+main {
+  text-align: center;
+  margin-top: 20%;
+}
+</style>
---
+++ playground/nuxt/pages/hovercard/index.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import {
+  HoverCardArrow,
+  HoverCardContent,
+  HoverCardPortal,
+  HoverCardRoot,
+  HoverCardTrigger,
+} from '@yi-ui/vue'
+
+const hoverState = ref(false)
+</script>
+
+<template>
+  <HoverCardRoot v-model:open="hoverState">
+    <HoverCardTrigger
+      href="https://twitter.com/radix_ui"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      悬浮鼠标
+    </HoverCardTrigger>
+    <HoverCardPortal>
+      <HoverCardContent
+        :side-offset="5"
+        as-child
+        data-vitest-content-wrapper="test-hover-card-content"
+      >
+        <div class="flex flex-col gap-[7px]">
+          <div class="flex flex-col gap-[15px]">
+            悬浮内容1
+          </div>
+          <div class="flex flex-col gap-[15px]">
+            悬浮内容2
+          </div>
+          <div class="flex flex-col gap-[15px]">
+            悬浮内容3
+          </div>
+          <div class="flex flex-col gap-[15px]">
+            悬浮内容4
+          </div>
+        </div>
+        <HoverCardArrow class="fill-white" :width="8" />
+      </HoverCardContent>
+    </HoverCardPortal>
+  </HoverCardRoot>
+</template>
+
+<style scoped>
+/* Your component styles here */
+</style>
